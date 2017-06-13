f7bdc8997c46d967928ad0fc269d5ef91168d707
THIAGO <thiago_loal@hotmail.com>
2017-06-13 13:11:06 -0300

    exercises

---
+++ ex.2/component.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default () => (
+
+	<h1>Primeiro Component!</h1>
+	)
+
+
--- ex/index.jsx
+++ ex/index.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Primeiro, { Segundo } from './component'
 
-ReactDOM.render(<h1>Olá React</h1>, document.getElementById('app'))
+ReactDOM.render(
+	<div>
+		<Primeiro />
+		<Segundo />
+	</div>
+, document.getElementById('app'))
 
